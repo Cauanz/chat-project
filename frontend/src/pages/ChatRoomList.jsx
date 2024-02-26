cf0bd7d113fc0da93a6229e1238a1f890688bb72
Cauanz <61758981+Cauanz@users.noreply.github.com>
2024-02-26 11:51:40 -0300

    🚚 💄 Transferir botão e lógica de criar chat de ChatRoom para ChatRoomList

--- frontend/src/pages/ChatRoomList.jsx
+++ frontend/src/pages/ChatRoomList.jsx
@@ -1,7 +1,12 @@
-export default function ChatRoomList() {
-  //TODO- LISTA DE CHATS QUE VAI FICAR NA ESQUERDA COM TODOS OS CHATS CRIADOS OU DISPONIVEIS PARA ENTRAR OU SOMENTE OS QUE VOCE JA ESTA
+import { Button, Modal, Checkbox, Form, Input, ConfigProvider } from 'antd';
+import { PlusCircleOutlined } from '@ant-design/icons';
+import { useFormStore } from '../store/FormStore';
+import { useRef, useState } from 'react';
+import axios from 'axios';
 
-  //TODO- Adicionar form pop-up para criação de novo chat, pergunta nome etc... disabilitar funcionalidade de clicar fora do pop-up e desaparecer
+export default function ChatRoomList() {
+  //*- LISTA DE CHATS QUE VAI FICAR NA ESQUERDA COM TODOS OS CHATS CRIADOS OU DISPONIVEIS PARA ENTRAR OU SOMENTE OS QUE VOCE JA ESTA
+  //*- E O BOTÃO PARA CRIAR NOVO CHAT, CHATROOM VAI TER MUITA COISA ENTÃO TRANSFERI PARA CÁ
 
   const chatRooms = [
     'Chat Room 1',
@@ -26,10 +31,125 @@ export default function ChatRoomList() {
     'Chat Room 10',
   ]; //! EXEMPLO PARA DEBUG
 
+  const [open, setOpen] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+  const [chat, setChat] = useState({
+    name: '',
+    description: '',
+    creator: {
+      id: '',
+      //SEM NOME POR ENQUANTO
+    },
+    participants: [],
+    messages: [],
+  });
+
+  const showModal = () => {
+    setOpen(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setConfirmLoading(true);
+    //OS DADOS SÃO ENVIADOS DEPOIS NO SETTIMEOUT O LOADING É DESATIVADO
+
+    setChat((prevState) => ({ ...prevState, name: name, description: description }));
+
+    //TODO CRIAR ROTA PARA CRIAR CHAT
+    /*     try {
+      const response = axios('http://localhost:3000/user/create', chat);
+    } catch (err) {
+      console.log(err);
+    } */
+    setTimeout(() => {
+      setOpen(false);
+      setConfirmLoading(false);
+      console.log(chat);
+    }, 2000);
+  };
+
+  const handleCancel = () => {
+    console.log('Clicked cancel button');
+    setOpen(false);
+  };
+
   return (
     <>
       <div className="flex h-full w-full">
         <div className="grid grid-cols-1 gap-4 overflow-y-auto w-full">
+          <div className="sticky top-0 z-50 bg-white shadow-md p-4 rounded-md flex items-center">
+            <ConfigProvider
+              theme={{
+                token: {
+                  colorPrimary: '#225bfe',
+                },
+              }}
+            >
+              <Button
+                type="primary"
+                className="w-14 h-14 bg-sky-700 rounded-3xl flex justify-center items-center mr-1"
+                onClick={showModal}
+              >
+                <PlusCircleOutlined style={{ fontSize: '40px' }} />
+              </Button>
+            </ConfigProvider>
+            <h2 className="font-bold uppercase">Criar chat</h2>
+          </div>
+          <Modal
+            title="Criar novo Chat"
+            open={open}
+            htmlType="submit"
+            confirmLoading={confirmLoading}
+            onCancel={handleCancel}
+            footer={null}
+          >
+            <form>
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+                  Nome
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="name"
+                  type="text"
+                  placeholder="Digite o nome"
+                  required
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                  name="name"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">
+                  Descrição
+                </label>
+                <textarea
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="description"
+                  placeholder="Digite a descrição"
+                  onChange={(e) => {
+                    setDescription(e.target.value);
+                  }}
+                  name="description"
+                ></textarea>
+              </div>
+              <div className="flex items-center justify-between">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                  onClick={handleSubmit}
+                >
+                  Criar
+                </button>
+              </div>
+            </form>
+          </Modal>
+          //* Lógica para mostrar chats
           {/* Replace this with your chat room cards */}
           {chatRooms.map((chatRoom, index) => (
             <div key={index} className="bg-white shadow-md p-4 rounded-md">
