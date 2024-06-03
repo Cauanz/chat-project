214f9de6f98d87dd2ce7101ba4e906b1d09f47ad
Cauanz <61758981+Cauanz@users.noreply.github.com>
2024-06-03 19:52:17 -0300

    💄 Criado messageForm, só, nada implementado, (não lembro a lógica)

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -1,10 +1,17 @@
 
-export default function MessageForm() {
+export default function MessageForm(handleMessageInput) {
 
    //TODO - FORMULARIO PARA MENSAGEM, COMO É UM CHAT O FORM É SÓ UM CAMPO DE TEXTO E TALVEZ UM BOTÃO PARA ENVIAR
+
+
+
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <form action="">
+        <input type="text" name="text" id="text" onChange={handleMessageInput} />
+        <button type="submit">Enviar</button>
+      </form>
+    </>
   )
 }
